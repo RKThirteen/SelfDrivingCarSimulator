98da9d8b6ed4a7173948a2d3cface1a96688133e
RK13 <eduard_barbu_13@yahoo.com>
2025-05-12 17:40:04 +0300

    fix: saved scene

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -12,6 +12,7 @@ public class CarController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0, -0.9f, 0);
     }
 
     void FixedUpdate()
